2e0d13f57c40c6f169194b8121b5f5da99989bb0
suchi jain <116373429+suchijain30@users.noreply.github.com>
2024-07-09 22:00:44 +0530

    Time: 143 ms (76.47%) | Memory: 76.5 MB (83.82%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -2,25 +2,15 @@ class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         double Waittime = 0; 
-        int currtime = 0; 
-
-        // for (const auto& customer : customers) {
-        //     int arrivaltime = customer[0]; 
-        //     int ordertime = customer[1]; 
-
-        //     currtime = max(currtime, arrivaltime) + ordertime;
-          
-        //     Waittime += currtime - arrivaltime;
-        // }
-        for (int i = 0; i < customers.size(); ++i) {
-    int arrivaltime = customers[i][0];
-    int ordertime = customers[i][1];
-
-    currtime = std::max(currtime, arrivaltime) + ordertime;
-
-    Waittime += currtime - arrivaltime;
-}
-
+        int currtime = 0;  
+        for (int i = 0; i < customers.size(); ++i) 
+        {
+            int arrivaltime = customers[i][0];
+            int ordertime = customers[i][1];
+            currtime = max(currtime, arrivaltime) + ordertime;
+            
+            Waittime += currtime - arrivaltime;
+        }
         return Waittime / customers.size();
     }
 };
