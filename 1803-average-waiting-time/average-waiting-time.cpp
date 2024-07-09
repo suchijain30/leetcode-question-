4091eabb54a5e61e5986ea2c6aa0c092deeb0af3
suchi jain <116373429+suchijain30@users.noreply.github.com>
2024-07-09 22:02:14 +0530

    Time: 143 ms (76.47%) | Memory: 76.6 MB (35.00%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         double Waittime = 0; 
-        int currtime = 0;  
+        int currtime = 0; 
         for (int i = 0; i < customers.size(); ++i) 
         {
             int arrivaltime = customers[i][0];
