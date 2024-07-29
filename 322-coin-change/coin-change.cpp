5cf21cae3e04d49bebbad16bac818008c17d29fd
suchi jain <116373429+suchijain30@users.noreply.github.com>
2024-07-29 21:57:37 +0530

    Time: 11 ms (99.12%) | Memory: 16.3 MB (71.78%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -22,40 +22,64 @@ public:
 //     }
 //     return mini ;
 // }
-int solve_using_memo(vector<int>& coins, int amount, vector<int>&dp)
+
+// int solve_using_memo(vector<int>& coins, int amount, vector<int>&dp)
+// {
+//     if(amount==0)
+//     {
+//         return 0 ;
+//     }
+//     if(dp[amount] != -1)
+//     {
+//         return dp[amount];
+//     }
+//     int mini = INT_MAX ;
+//     for(int i=0 ; i<coins.size(); i++)
+//     {
+//         if(coins[i] <= amount)
+//         {
+//             int recans = solve_using_memo(coins, amount-coins[i], dp); 
+//             if(recans != INT_MAX)
+//             {
+//             int ans = 1+ recans; // 1 one means 1coin add humne kar lia baki rec dega
+//                 mini = min(mini , ans);
+//             }
+//         }
+        
+//     }
+//     dp[amount]= mini  ;
+//     return dp[amount] ;
+// }
+int solve_usig_tabulation(vector<int>& coins, int amount)
 {
-    if(amount==0)
+    int n = amount ;
+    vector<int>dp(n+1 , INT_MAX);  // step 1 dp array banao
+    dp[0]= 0 ;                // step2 analyaze base acse or find value to give to dp
+    for(int value =1 ; value<= amount ; value++)
     {
-        return 0 ;
-    }
-    if(dp[amount] != -1)
-    {
-        return dp[amount];
-    }
-    int mini = INT_MAX ;
-    for(int i=0 ; i<coins.size(); i++)
-    {
-        if(coins[i] <= amount)
+       int mini = INT_MAX ;
+        for(int i=0 ; i<coins.size(); i++)
         {
-            int recans = solve_using_memo(coins, amount-coins[i], dp); 
-            if(recans != INT_MAX)
+            if(value- coins[i] >=0 )
             {
-            int ans = 1+ recans; // 1 one means 1coin add humne kar lia baki rec dega
-                mini = min(mini , ans);
+                int recans = dp[value-coins[i] ]; 
+                if(recans != INT_MAX)
+                {
+                    int ans = 1+ recans; 
+                    mini = min(mini , ans);
+                }
             }
         }
-        
+         dp[value]= mini  ;
     }
-    dp[amount]= mini  ;
-    return dp[amount] ;
+    return dp[amount] ; 
 }
     int coinChange(vector<int>& coins, int amount) {
         // step 1 make a dp vector
-        int n = amount+1; 
-        vector<int>dp(n , -1);
-        int res = solve_using_memo(coins, amount, dp);
-
-
+        // int n = amount+1; 
+        // vector<int>dp(n+1 , -1);
+        // int res = solve_using_memo(coins, amount, dp);
+        int res = solve_usig_tabulation(coins,amount) ;
 
         // int res = recusionsolve(coins,  amount);
         if(res==INT_MAX)
