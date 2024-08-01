299ee520de76b845873d39ca3b57bb47332c9943
suchi jain <116373429+suchijain30@users.noreply.github.com>
2024-08-01 20:37:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (27.35%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
+int convert(string s) 
+{ 
+    int num = 0; 
+    int n = s.length(); 
+    for (int i = 0; i < n; i++) 
+    {
+        // Subtract 48 from the current digit 
+        num = num * 10 + (int(s[i]) - 48); 
+    }
+    return num;
+} 
     int countSeniors(vector<string>& details) {
         int count =0 ;
         for(int i=0 ; i<details.size() ; i++)
         {
             string a = details[i];
             string substring = a.substr(11, 2);
-            int b = stoi(substring);
+            int b = convert(substring);
             if(b>60)
             {
                 count++;
