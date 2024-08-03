23733c57776daed6d6d1efc8f564e84ba8c8667f
suchi jain <116373429+suchijain30@users.noreply.github.com>
2024-08-04 00:32:58 +0530

    Time: 3 ms (37.99%) | Memory: 8.3 MB (38.30%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+         int n = nums.size();
+         vector<string> ans;
+        if(n == 0)
+        { 
+            return ans ;
+        } 
+        int start;
+        for(int i = 0; i < n; i++)
+        {
+            start = nums[i];
+            while(i+1 < n && nums[i] != INT_MAX && nums[i+1] == nums[i]+1)
+            {
+                i++;
+            }
+            //now i is the index of the end of the interval
+            
+            if(start == nums[i])
+                ans.push_back(to_string(start));
+            else
+                ans.push_back(to_string(start) + "->" + to_string(nums[i]));
+        }
+        
+        return ans;
+    }
+};
