4b724aa9a58293b5722a46c3ab41476a43ce26ca
suchi jain <116373429+suchijain30@users.noreply.github.com>
2025-12-29 23:43:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (28.26%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    vector<int> countBits(int n) {
-        vector<int> ans(n + 1);
-        ans[0] = 0;
-        for (int i = 1; i <= n; i++) {
-            // If 'i' is even: same number of 1s as i / 2
-            // If 'i' is odd: one more 1 than i / 2
-            ans[i] = ans[i / 2] + (i % 2);
+    vector<int> countBits(int n) 
+    {
+        vector<int> ans;
+        for (int i = 0; i <= n; i++)
+        {
+            int x = i;      // copy of i
+            int count = 0;
+
+            while (x > 0) 
+            {
+                x = x & (x - 1);
+                count++;
+            }
+            ans.push_back(count);
         }
-        return ans;
+    return ans ;
     }
 };
