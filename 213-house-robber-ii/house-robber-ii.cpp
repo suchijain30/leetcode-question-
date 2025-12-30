11706a3460190797593320b137d4f366b4515557
suchi jain <116373429+suchijain30@users.noreply.github.com>
2025-12-30 19:44:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (37.10%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+int solveusingmemoa(vector<int>&dp1 , int index, vector<int>&nums1)
+{
+    if(index>=nums1.size())
+    {
+        return 0;
+    }
+    if(dp1[index]!= -1)
+    {
+        return dp1[index];
+    }
+    int option1 = nums1[index]+solveusingmemoa(dp1,index+2,nums1);//including first value
+    int option2 = 0 + solveusingmemoa(dp1, index+1, nums1); // excluding first value
+    dp1[index]=max(option1 , option2);
+    return dp1[index];         // index is changing not n 
+}
+int solveusingmemob(vector<int>&dp2 , int index, vector<int>&nums2)
+{
+    if(index>=nums2.size())
+    {
+        return 0;
+    }
+    if(dp2[index]!= -1)
+    {
+        return dp2[index];
+    }
+    int option1 = nums2[index]+solveusingmemob(dp2,index+2,nums2);//including first value
+    int option2 = 0 + solveusingmemob(dp2, index+1, nums2); // excluding first value
+    dp2[index]=max(option1 , option2);
+    return dp2[index];         // index is changing not n 
+}
+int rob(vector<int>& nums) {
+       int n = nums.size();
+       if(n==1)
+       {
+        return nums[0];
+       }
+       vector<int>nums1;
+       vector<int>nums2;
+       for(int i=0 ; i<n-1; i++)
+       {
+        nums1.push_back(nums[i]);
+       }
+       for(int i=1 ; i<n ; i++)
+       {
+        nums2.push_back(nums[i]);
+       }
+       int index =0 ;
+       vector<int>dp1(n ,-1);
+       vector<int>dp2(n ,-1);
+       int ansa = solveusingmemoa(dp1 , index, nums1);
+       int ansb = solveusingmemob(dp2 , index, nums2);
+       int ans = max(ansa, ansb);
+       return ans;
+    }
+};
